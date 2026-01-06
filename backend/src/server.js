e08ca410520d11f000bb393934c798d9576c56f3
sarojkumarvn <sarojojha2457@gmail.com>
2026-01-06 21:08:29 +0530

    Backend ready for Railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import dotenv from "dotenv";
 import cors from "cors";
 import { serve } from "inngest/express";
@@ -7,40 +6,36 @@ import { serve } from "inngest/express";
 import { ENV } from "./lib/env.js";
 import connectDB from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
+
 dotenv.config();
 
 const app = express();
-const __dirname = path.resolve();
 
-// server allows a browser to include cookies on requests
-app.use(cors({origin: ENV.CLIENT_URL , credentials:true}));
+
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/inngest" , serve({client:inngest , functions}))
-
-
-//making the app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  //Any routes not handled before will be handled here
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// Inngest endpoint (VERY IMPORTANT)
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-      console.log("Server listening on ", ENV.PORT);
+
+    const PORT = ENV.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log("🚀 Server listening on", PORT);
     });
   } catch (error) {
-    console.error(err);
+    console.error("❌ Server failed to start:", error);
   }
 };
-
 
 startServer();
